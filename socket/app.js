4e975f900d997495f6a1bc42b283858376df9ce8
Cyriaque Delaunay <cyriaquedelaunay@gmail.com>
2016-02-22 17:02:46 +0100

    Updating socket method (Wip)

--- socket/app.js
+++ socket/app.js
@@ -12,22 +12,22 @@ app.listen(port,function(){
   console.log("Socket server listening on port "+port);
 });
 
-var rooms = {};
+var channels = {};
 
 io.on('connection', function (socket) {
-	var currentRoom;
+	var currentChannel;
 	socket.user = {
 		"nickname" : "Anonymous"
 	};
 
 	socket.on('disconnect', function()
 	{
-		socket.broadcast.to(currentRoom).emit("userLeave",{user:socket.user.id});
-    if(rooms[currentRoom])
+		socket.broadcast.to(currentChannel).emit("userLeave",{user:socket.user.id});
+    if(channels[currentChannel])
     {
-      rooms[currentRoom].users.splice(rooms[currentRoom].users.indexOf(socket.user.id), 1);
+      channels[currentChannel].users.splice(channels[currentChannel].users.indexOf(socket.user.id), 1);
     }
-		console.log(socket.user.nickname+" leave "+currentRoom);
+		console.log(socket.user.nickname+" leave "+currentChannel);
 	});
 
   	socket.on("userAuth",function(d)
@@ -50,115 +50,57 @@ io.on('connection', function (socket) {
   		console.log(err);
   	});
 
-  	socket.on("joinRoomRequest",function(d)
+  	socket.on("joinChannel",function(d)
   	{
-  		currentRoom = d.room;
+  		currentChannel = d.channel;
 
-  		if(!rooms.hasOwnProperty(currentRoom))		//room doesnt exist : create it
+  		if(!channels.hasOwnProperty(currentChannel))		//room doesnt exist : create it
   		{
-  			rooms[currentRoom] = {
-	  			"name" : currentRoom,
+  			channels[currentChannel] = {
+	  			"name" : currentChannel,
 	  			users : new Array()
 	  		};
   		}
   		
 
-  		socket.join(currentRoom);
+  		socket.join(currentChannel);
   		
-  		if(rooms[currentRoom].users.length > 0)
+  		if(channels[currentChannel].users.length > 0)
   		{
-  			for (var i = 0; i < rooms[currentRoom].users.length; i++)
+  			for (var i = 0; i < channels[currentChannel].users.length; i++)
   			{
-  				
-  				socket.emit("userJoin",{user:rooms[currentRoom].users[i]});
+  				socket.emit("userJoin",{user:channels[currentChannel].users[i]});
   			}
   		}
-  		rooms[currentRoom].users.push(socket.user.id);
+  		channels[currentChannel].users.push(socket.user.id);
 
-  		console.log(socket.user.nickname+" join "+currentRoom);
-  		socket.broadcast.to(currentRoom).emit("userJoin",{user:socket.user.id});
+  		console.log(socket.user.nickname+" join "+currentChannel);
+  		socket.broadcast.to(currentChannel).emit("userJoin",{user:socket.user.id});
   	});
 
   	socket.on("chat",function(data)
   	{
   		var message = data.message;
   		/* checking message content aud auth */
-  		io.to(currentRoom).emit("chat",{event:data.event,message:message,user:socket.user});
-  		console.log(socket.user.nickname+' (in '+currentRoom+') "'+message+'"');
+  		io.to(currentChannel).emit("chat",{event:data.event,message:message,user:socket.user});
+  		console.log(socket.user.nickname+' (in '+currentChannel+') "'+message+'"');
   	});
 
-    socket.on("subtitle",function(data)
+    socket.on("channelEvent",function(data,channelId)   //send an event to all clients connected to the channel
     {
-      var s = data.subtitle;
-      var valid = true;
-
-      subtitleManager[data.event](data,socket);
+      channelManager[data.event](data,socket);
     });
 
-    var subtitleManager = {
-      add : function(data,socket)
+    var channelManager = {
+      setFile : function(data)
       {
-        var postData = data.subtitle;
-        var uri = apiIndex+"/subtitles?token="+data.auth_token;
-        request.post({url:uri, form: postData,json:true}, function(error,response,body){
+        io.to(currentChannel).emit("channel",{event:data.event,url:data.url,time:data.time});
+      },
 
-          if (!error && String(response.statusCode).charAt(0) == 2) { //2XX
-            
-            // if subtitle added, retreive his informations and send them to all clients
-            var subtitleId = body.message;
-            request.get(apiIndex+"/subtitles/"+subtitleId,function(serror,sresponse,sbody){
-              sbody = JSON.parse(sbody);
-              if (!serror && String(sresponse.statusCode).charAt(0) == 2) //send all subtitle info
-              {
-                io.to(currentRoom).emit("subtitle",{event:data.event,subtitle:sbody.subtitles[0]});
-              }
-              else
-              {
-                console.log(uri);
-                console.log("Api error "+sresponse.statusCode);
-                console.log(sbody);
-              }
-            });            
-          }
-          else
-          {
-            console.log(uri);
-            console.log("Api error "+response.statusCode);
-            console.log(body);
-          }
-        });
-      },
-      remove : function(data,socket)
+      setBanner : function(data)
       {
-        var subtitleId = data.id;
-        var uri = apiIndex+"/subtitles/"+subtitleId+"?token="+data.auth_token;
-        request.del({url:uri,json:true}, function(error,response,body){
-          if (!error && String(response.statusCode).charAt(0) == 2) {
-            socket.broadcast.to(currentRoom).emit("subtitle",{event:data.event,id:subtitleId});  //send a message to all user to remove the subtitle "x"            
-          }
-          else
-          {
-            console.log(uri);
-            console.log("Api error "+response.statusCode);
-            console.log(body);
-          }
-        });
-      },
-      edit : function(data,socket)
-      {
-        var postData = data;
-        var uri = apiIndex+"/subtitles/"+postData.subtitle.id+"?token="+data.auth_token;
-        request.put({url:uri, form: postData.subtitle,json:true}, function(error,response,body){
-          if (!error && String(response.statusCode).charAt(0) == 2) {
-            socket.broadcast.to(currentRoom).emit("subtitle",{event:data.event,subtitle:postData.subtitle});  //send a message to all user to remove the subtitle "x"            
-          }
-          else
-          {
-            console.log(uri);
-            console.log("Api error "+response.statusCode);
-            console.log(body);
-          }
-        });
+        io.to(currentChannel).emit("channel",{event:data.event,text:data.text,show:data.show});
       }
-    };
+
+    }
 });
